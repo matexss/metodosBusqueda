72c863a77529d299b738fdf5c2de8a7cc07d4ac8
Mateo <mateomorejon993@gmail.com>
2025-04-15 09:05:32 -0500

    Commit

--- src/App.java
+++ src/App.java
@@ -1,11 +1,19 @@
 import controllers.MetodosBusqueda;
+import models.Persona;
 
 public class App {
     public static void main(String[] args) {
-        MetodosBusqueda metodosBusqueda = new MetodosBusqueda();
-        int[] arreglo = {3, 8, 15, 22, 35};
-        int valorBuscado = 22;
 
-        metodosBusqueda.busquedaLineal(arreglo, valorBuscado);
+        Persona[] personas = new Persona[7];
+        personas[0] = new Persona(101, "Ana");
+        personas[1] = new Persona(102, "Luis");
+        personas[2] = new Persona(103, "María");
+        personas[3] = new Persona(104, "Pedro");
+        personas[4] = new Persona(105, "Lucía");
+        personas[5] = new Persona(106, "Carlos");
+        personas[6] = new Persona(107, "Sofía");
+        MetodosBusqueda mB = new MetodosBusqueda(personas);
+
     }
-}
+
+    }
---
+++ src/controllers/MetodosBusqueda.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import views.ShowConsole;
+import models.Persona;
+
+public class MetodosBusqueda {
+
+    private ShowConsole showConsole;
+    private Persona[] people;
+
+    public MetodosBusqueda(Persona[] persons) {
+        this.showConsole = new ShowConsole();
+        this.people = persons;
+    }
+
+    public int busquedaLineal(int[] arreglo, int valorBuscado) {
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == valorBuscado) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int findPersonaByCode(int code) {
+        for (int i = 0; i < people.length; i++) {
+            if (people[i] != null && people[i].getCode() == code) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void showPersonByCode() {
+        int codeToFind = showConsole.getCode();  // Asegurate de que este método exista
+        int indexPerson = findPersonaByCode(codeToFind);
+        
+        if (indexPerson >= 0) {
+            showConsole.showMessage("Persona encontrada:");
+            showConsole.showMessage(people[indexPerson].toString());
+        } else {
+            showConsole.showMessage("Persona con código " + codeToFind + " no encontrada.");
+        }
+    }
+}
